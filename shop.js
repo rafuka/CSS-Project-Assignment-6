18c542e886e16f40e82d6c95445e2ee0edb2dcce
Rafael Garcia <rafuk89@gmail.com>
2017-01-26 21:54:33 +0100

    added the cart as a popoup

--- shop.js
+++ shop.js
@@ -1,3 +1,19 @@
 var shopItems = document.getElementsByClassName("product");
 console.log(shopItems);
 
+var cartBtn = document.getElementById("cart-btn");
+var cart = document.getElementById("cart");
+
+cartBtn.addEventListener("click", openCart, false);
+
+function openCart(e) {
+
+	cart.classList.add("visible");
+	document.body.classList.add("popup");
+}
+
+function closeCart(e) {
+
+	cart.classList.remove("visible");
+	document.body.classList.remove("popup");
+}
