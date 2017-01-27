b58c41d949b4f51856f768294ce4f3af2da92040
Rafael Garcia <rafuk89@gmail.com>
2017-01-27 18:54:38 +0100

    checkpoint

--- shop.js
+++ shop.js
@@ -1,24 +1,11 @@
 var shopItems = document.getElementsByClassName("product");
 console.log(shopItems);
 
-var cart = document.getElementById("cart");
+var cart = document.getElementById("cart-container");
+var shop = document.getElementById("grid");
 var openCartBtn = document.getElementById("open-cart-btn");
 var closeCartBtn = document.getElementById("close-cart-btn");
-
-function openCart(e) {
-
-	cart.classList.add("visible");
-	document.body.classList.add("popup");
-}
-
-function closeCart(e) {
-
-	cart.classList.remove("visible");
-	document.body.classList.remove("popup");
-}		
-
-openCartBtn.addEventListener("click", openCart, false);
-closeCartBtn.addEventListener("click", closeCart, false);
+var coupon = "BLAHBLAH";
 
 var cartList = [
 	{
@@ -38,3 +25,36 @@ var cartList = [
 	{},
 	{}
 ];
+
+shop.addEventListener("click", function(e) {
+	if (e.target.classList.contains('add-to-cart')) {
+		console.log("ADD");
+
+		
+		var shopElement = e.target.parentNode;
+		console.log(shopElement.id);
+
+		console.log(shopElement.querySelector('.product-title').innerHTML);
+	}
+});
+
+function openCart(e) {
+
+	cart.classList.add("visible");
+	document.body.classList.add("popup");
+}
+
+function closeCart(e) {
+
+	cart.classList.remove("visible");
+	document.body.classList.remove("popup");
+}		
+
+function addToCart(item) {
+
+}
+
+
+openCartBtn.addEventListener("click", openCart, false);
+closeCartBtn.addEventListener("click", closeCart, false);
+
