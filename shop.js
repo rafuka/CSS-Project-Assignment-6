944a33e14724affceea5af7ba37e84b6522abd5a
Rafael Garcia <rafuk89@gmail.com>
2017-01-29 15:46:31 +0100

    checkpoint

--- shop.js
+++ shop.js
@@ -1,7 +1,5 @@
 var shopItems = document.getElementsByClassName("product");
-console.log(shopItems);
-
-var cart = document.getElementById("cart-container");
+var cart = document.getElementById("cart");
 var shop = document.getElementById("grid");
 var openCartBtn = document.getElementById("open-cart-btn");
 var closeCartBtn = document.getElementById("close-cart-btn");
@@ -9,34 +7,14 @@ var coupon = "BLAHBLAH";
 
 var cartList = [
 	{
+		id: 'product-1',
 		title: 'Buggawats',
 		desc: '',
 		price: '',
 		img: '',
-		qty: 0
-	},
-	{},
-	{},
-	{},
-	{},
-	{},
-	{},
-	{},
-	{},
-	{}
-];
-
-shop.addEventListener("click", function(e) {
-	if (e.target.classList.contains('add-to-cart')) {
-		console.log("ADD");
-
-		
-		var shopElement = e.target.parentNode;
-		console.log(shopElement.id);
-
-		console.log(shopElement.querySelector('.product-title').innerHTML);
+		qty: 1
 	}
-});
+];
 
 function openCart(e) {
 
@@ -50,11 +28,43 @@ function closeCart(e) {
 	document.body.classList.remove("popup");
 }		
 
-function addToCart(item) {
+function addToCart(shopElement) {
 
+	for (var i in cartList) {
+
+		if (cartList[i].id == shopElement.id) {
+			cartList[i].qty++;
+			return;
+		}
+	}
+
+	cartList.push({
+
+		id: shopElement.id,
+		title: shopElement.querySelector('.product-title').innerHTML,
+		img: shopElement.querySelector('.product-img').getAttribute('src'),
+		desc: shopElement.querySelector('.product-description').innerHTML,
+		qty: 1
+	});
+
+	return;
+}
+
+function removeFromCart(cartItem) {
+	
 }
 
 
 openCartBtn.addEventListener("click", openCart, false);
 closeCartBtn.addEventListener("click", closeCart, false);
 
+shop.addEventListener("click", function(e) {
+	if (e.target.classList.contains('add-to-cart')) {
+		
+		var shopElement = e.target.parentNode;
+		console.log(shopElement.id);
+		addToCart(shopElement);
+		console.log(cartList);
+
+	}
+});
