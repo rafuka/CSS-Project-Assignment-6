d31126c6e7252164a2b218478d34aa859a9641e8
Rafael Garcia <rafuk89@gmail.com>
2018-07-26 22:29:48 +0700

    added cart functionality

--- dist/scripts/built.js
+++ dist/scripts/built.js
@@ -1,7 +1,10 @@
 (function ($, Handlebars, TweenMax) {
 
-	var shopData = null;	// will contain the shop items' data.
-	var cartData = null;	// will contain the cart items' data.
+	var shopData = {};	// will contain the shop items' data.
+	var cartData = {};	// will contain the cart items' data.
+	var totalCartItems = 0;
+	var totalPriceNum = 0;
+	var totalPriceElem = $('#total-price');
 
 	var shopTemplateHTML = document.getElementById('shop-template').innerHTML;
 	var shopTemplate = Handlebars.compile(shopTemplateHTML);
@@ -9,54 +12,147 @@
 	var cartTemplateHTML = document.getElementById('cart-template').innerHTML;
 	var cartTemplate = Handlebars.compile(cartTemplateHTML);
 
-	$.getJSON("shopdata.json", function(data) {
-
-		shopData = data;
-		console.log(shopData);
-		var shopHTML = shopTemplate(data);
-		document.getElementById('shop-list').innerHTML += shopHTML;
-
-		
-	});
-
 	var shopList = $('#shop-list');
 	var cart = $('#cart');
 	var cartList = $('#cart-list');
 	var overlay = $('#modal-overlay');
+	var cartNumElm = $('#cart-num');
 
-	$('body').on('click', '.cart-toggle', function() {
 
-		var duration = .7;
-		if (cart.hasClass('visible')) {
-			TweenMax.to('#cart', duration, {left: '100%'});
-			cart.removeClass('visible');
-			overlay.removeClass('cart-toggle');
+	// Retrieve cart element's from local storage (if any) and produce the HTML
+	if (localStorage.getItem('cartData')) {
 
-			TweenMax.to(overlay, duration, {display: 'none', opacity: 0});
-			$('body').removeClass('no-scroll');
-			
+		cartData = JSON.parse(localStorage.getItem('cartData'));
+
+		// Get total items in the cart & update cartNum Element.
+		for(var i = 0, len = cartData.items.length; i < len; i++) {
+			totalCartItems += cartData.items[i].qty;
+			cartNumElm.text(totalCartItems);
 		}
-		else {
-			TweenMax.to('#cart', duration, {left: '50%'});
-			cart.addClass('visible');
-			overlay.addClass('cart-toggle');
 
-			TweenMax.to(overlay, duration, {display: 'block', opacity: .8});
-			$('body').addClass('no-scroll');
-			// disable scroll in body
+		totalPriceNum = calculateTotalPrice();
+		totalPriceElem.text(totalPriceNum.toFixed(2));
+		var cartHTML = cartTemplate(cartData);
+		document.getElementById('cart-list').innerHTML = cartHTML;
+
+	}
+	else {
+		cartData = {
+			items: []
 		}
-		
+	}
+
+	// Read the shop listing item's data and produce the HTML
+	$.getJSON("shopdata.json", function(data) {
+
+		shopData = data;
+		var shopHTML = shopTemplate(shopData);
+		document.getElementById('shop-list').innerHTML += shopHTML;
+
+
+		var cartIcon = $('#cart-icon');
+
+		shopList.on('click', '.shop-item__add-btn', function(e) {
+
+			if (!TweenMax.isTweening(cartIcon)) {
+				TweenMax.from(cartIcon, .7, {scale: 1.5, ease: Bounce.easeOut });
+			}
+
+			var shopItem = $(e.target).closest('.shop-item');
+			var shopItemId = shopItem.attr('id');
+			var idNum = parseInt(shopItemId.replace(/[^0-9]/gi, ''));
+
+
+			// Check if item exists in cart. If not, create it and run template.
+			// Else just increase qty.
+
+			var itemInCart = false;
+			for (var i = 0, len = cartData.items.length; i < len; i++) {
+
+				if (cartData.items[i].id == idNum) {
+					cartData.items[i].qty++;
+					itemInCart = true;
+					console.log(cartData);
+				}
+			}
+
+			if(!itemInCart) {
+
+				var shopItemData;
+				for (var i = 0, len = shopData.items.length; i < len; i++) {
+					if (shopData.items[i].id == idNum) {
+						shopItemData = shopData.items[i];
+					}
+				}
+
+				shopItemData.qty = 1;
+				cartData.items.push(shopItemData);	
+			}
+
+			// Set total price & Produce cart's HTML
+			totalPriceNum = calculateTotalPrice();
+			totalPriceElem.text(totalPriceNum.toFixed(2));
+			var cartHTML = cartTemplate(cartData);
+			document.getElementById('cart-list').innerHTML = cartHTML;
+
+			totalCartItems++;
+			cartNumElm.text(totalCartItems);
+
+			localStorage.setItem('cartData', JSON.stringify(cartData));
+		});
 	});
 
-	shopList.on('click', '.shop-item__add-btn', function() {
-		console.log('buy!');
-	});
 
-	shopList.on('click', '.shop-item__details', function() {
+	// Open/Close Cart view.
+	$('body').on('click', '.cart-toggle', toggleCart);
+
+
+	shopList.on('click', '.shop-item__details', function(e) {
 		console.log('details');
 	});
 
-	console.log(shopList);
+
+	// --------- Functions --------- //
+
+	function toggleCart(e) {
+
+		var cartDuration = .7;
+		var overlayDuration = .5;
+
+		if (cart.hasClass('visible')) {
+			if (!TweenMax.isTweening(cart)) {
+				TweenMax.to(cart, cartDuration, {right: '-650px', ease: Back.easeIn.config(1.2)});
+				cart.removeClass('visible');
+				overlay.removeClass('cart-toggle');
+
+				TweenMax.to(overlay, overlayDuration, {display: 'none', opacity: 0});
+				$('body').removeClass('no-scroll');
+			}
+			
+		}
+		else {
+			if (!TweenMax.isTweening(cart)) {
+				TweenMax.to(cart, cartDuration, {right: '-50px', ease: Back.easeOut.config(1.2)});
+				cart.addClass('visible');
+				overlay.addClass('cart-toggle');
+
+				TweenMax.to(overlay, overlayDuration, {display: 'block', opacity: .8});
+				$('body').addClass('no-scroll');
+			}
+		}
+	}
+
+
+	// Return total price of items in the cart.
+	function calculateTotalPrice() {
+		price = 0;
+		for (var i = 0, len = cartData.items.length; i < len; i++) {
+			var cartItem = cartData.items[i];
+			price += cartItem.price * cartItem.qty;
+		}
+		console.log(price);
+		return price;
+	}
 
 	
 })(jQuery, Handlebars, TweenMax);
